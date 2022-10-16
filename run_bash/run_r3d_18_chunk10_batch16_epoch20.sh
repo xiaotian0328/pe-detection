fcc28673eb47ff1a2ea0ae991fec2b4a792e2d0b
Xiaotian Ma <44010854+xiaotian0328@users.noreply.github.com>
2022-10-15 23:15:14 -0500

    Update run_r3d_18_chunk10_batch16_epoch20.sh

--- run_bash/run_r3d_18_chunk10_batch16_epoch20.sh
+++ run_bash/run_r3d_18_chunk10_batch16_epoch20.sh
@@ -1,5 +1,5 @@
 python train_r3d_18.py \
-    --log_file="new/train_r3d_18_chunk10_batch16_epoch20_new_splits.log" \
+    --log_file="train_r3d_18_chunk10_batch16_epoch20.log" \
     --path="data/train/*/*" \
     --gpu=0 \
     --batch_size=16 \
@@ -7,4 +7,4 @@ python train_r3d_18.py \
     --learning_rate=0.0004 \
     --pos_weight=1. \
     --num_epoch=20 \
-    --model_dir="weights/new/r3d_18_chunk10_batch16_new_splits"
+    --model_dir="weights/r3d_18_chunk10_batch16"
